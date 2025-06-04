11fd357bd5295612bcf43760f50a9c5bef0a866d
Omar Alaa <omar98alaa@gmail.com>
2025-06-05 02:19:51 +0300

    Dashboard form

---
+++ Forms/DashboardForm.Designer.cs
@@ -0,0 +1,438 @@
+﻿namespace Warehouse_Management_App.Forms
+{
+    partial class DashboardForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label19 = new Label();
+            panel8 = new Panel();
+            ClientsCountLabel = new Label();
+            label10 = new Label();
+            panel11 = new Panel();
+            ExportsCountLabel = new Label();
+            label16 = new Label();
+            panel7 = new Panel();
+            SuppliersCountLabel = new Label();
+            label8 = new Label();
+            panel12 = new Panel();
+            ImportsCountLabel = new Label();
+            label18 = new Label();
+            panel6 = new Panel();
+            StockCountLabel = new Label();
+            label6 = new Label();
+            panel5 = new Panel();
+            WarehousesCountLabel = new Label();
+            label3 = new Label();
+            label1 = new Label();
+            WarehouseReportButton = new Button();
+            ProductReportButton = new Button();
+            TransactionsFormButton = new Button();
+            StockReportFormButton = new Button();
+            panel8.SuspendLayout();
+            panel11.SuspendLayout();
+            panel7.SuspendLayout();
+            panel12.SuspendLayout();
+            panel6.SuspendLayout();
+            panel5.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label19
+            // 
+            label19.AutoSize = true;
+            label19.Font = new Font("Verdana", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label19.ForeColor = Color.White;
+            label19.Location = new Point(53, 50);
+            label19.Name = "label19";
+            label19.Size = new Size(157, 32);
+            label19.TabIndex = 17;
+            label19.Text = "Overview";
+            // 
+            // panel8
+            // 
+            panel8.BackColor = Color.FromArgb(45, 62, 80);
+            panel8.BorderStyle = BorderStyle.FixedSingle;
+            panel8.Controls.Add(ClientsCountLabel);
+            panel8.Controls.Add(label10);
+            panel8.Font = new Font("Verdana", 12F, FontStyle.Bold);
+            panel8.Location = new Point(668, 115);
+            panel8.Name = "panel8";
+            panel8.Padding = new Padding(15);
+            panel8.Size = new Size(250, 90);
+            panel8.TabIndex = 8;
+            // 
+            // ClientsCountLabel
+            // 
+            ClientsCountLabel.Dock = DockStyle.Bottom;
+            ClientsCountLabel.Font = new Font("Verdana", 15.75F, FontStyle.Bold);
+            ClientsCountLabel.ForeColor = Color.FromArgb(27, 188, 155);
+            ClientsCountLabel.Location = new Point(15, 38);
+            ClientsCountLabel.Name = "ClientsCountLabel";
+            ClientsCountLabel.Size = new Size(218, 35);
+            ClientsCountLabel.TabIndex = 1;
+            ClientsCountLabel.Text = "100";
+            ClientsCountLabel.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Dock = DockStyle.Top;
+            label10.ForeColor = Color.White;
+            label10.Location = new Point(15, 15);
+            label10.Name = "label10";
+            label10.Size = new Size(66, 18);
+            label10.TabIndex = 0;
+            label10.Text = "Clients";
+            label10.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // panel11
+            // 
+            panel11.BackColor = Color.FromArgb(45, 62, 80);
+            panel11.BorderStyle = BorderStyle.FixedSingle;
+            panel11.Controls.Add(ExportsCountLabel);
+            panel11.Controls.Add(label16);
+            panel11.Font = new Font("Verdana", 12F, FontStyle.Bold);
+            panel11.Location = new Point(668, 221);
+            panel11.Name = "panel11";
+            panel11.Padding = new Padding(15);
+            panel11.Size = new Size(250, 90);
+            panel11.TabIndex = 14;
+            // 
+            // ExportsCountLabel
+            // 
+            ExportsCountLabel.Dock = DockStyle.Bottom;
+            ExportsCountLabel.Font = new Font("Verdana", 15.75F, FontStyle.Bold);
+            ExportsCountLabel.ForeColor = Color.FromArgb(27, 188, 155);
+            ExportsCountLabel.Location = new Point(15, 38);
+            ExportsCountLabel.Name = "ExportsCountLabel";
+            ExportsCountLabel.Size = new Size(218, 35);
+            ExportsCountLabel.TabIndex = 1;
+            ExportsCountLabel.Text = "100";
+            ExportsCountLabel.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.Dock = DockStyle.Top;
+            label16.ForeColor = Color.White;
+            label16.Location = new Point(15, 15);
+            label16.Name = "label16";
+            label16.Size = new Size(76, 18);
+            label16.TabIndex = 0;
+            label16.Text = "Exports";
+            label16.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // panel7
+            // 
+            panel7.BackColor = Color.FromArgb(45, 62, 80);
+            panel7.BorderStyle = BorderStyle.FixedSingle;
+            panel7.Controls.Add(SuppliersCountLabel);
+            panel7.Controls.Add(label8);
+            panel7.Font = new Font("Verdana", 12F, FontStyle.Bold);
+            panel7.Location = new Point(390, 115);
+            panel7.Name = "panel7";
+            panel7.Padding = new Padding(15);
+            panel7.Size = new Size(250, 90);
+            panel7.TabIndex = 9;
+            // 
+            // SuppliersCountLabel
+            // 
+            SuppliersCountLabel.Dock = DockStyle.Bottom;
+            SuppliersCountLabel.Font = new Font("Verdana", 15.75F, FontStyle.Bold);
+            SuppliersCountLabel.ForeColor = Color.FromArgb(27, 188, 155);
+            SuppliersCountLabel.Location = new Point(15, 38);
+            SuppliersCountLabel.Name = "SuppliersCountLabel";
+            SuppliersCountLabel.Size = new Size(218, 35);
+            SuppliersCountLabel.TabIndex = 1;
+            SuppliersCountLabel.Text = "100";
+            SuppliersCountLabel.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Dock = DockStyle.Top;
+            label8.ForeColor = Color.White;
+            label8.Location = new Point(15, 15);
+            label8.Name = "label8";
+            label8.Size = new Size(88, 18);
+            label8.TabIndex = 0;
+            label8.Text = "Suppliers";
+            label8.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // panel12
+            // 
+            panel12.BackColor = Color.FromArgb(45, 62, 80);
+            panel12.BorderStyle = BorderStyle.FixedSingle;
+            panel12.Controls.Add(ImportsCountLabel);
+            panel12.Controls.Add(label18);
+            panel12.Font = new Font("Verdana", 12F, FontStyle.Bold);
+            panel12.Location = new Point(390, 221);
+            panel12.Name = "panel12";
+            panel12.Padding = new Padding(15);
+            panel12.Size = new Size(250, 90);
+            panel12.TabIndex = 15;
+            // 
+            // ImportsCountLabel
+            // 
+            ImportsCountLabel.Dock = DockStyle.Bottom;
+            ImportsCountLabel.Font = new Font("Verdana", 15.75F, FontStyle.Bold);
+            ImportsCountLabel.ForeColor = Color.FromArgb(27, 188, 155);
+            ImportsCountLabel.Location = new Point(15, 38);
+            ImportsCountLabel.Name = "ImportsCountLabel";
+            ImportsCountLabel.Size = new Size(218, 35);
+            ImportsCountLabel.TabIndex = 1;
+            ImportsCountLabel.Text = "100";
+            ImportsCountLabel.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // label18
+            // 
+            label18.AutoSize = true;
+            label18.Dock = DockStyle.Top;
+            label18.ForeColor = Color.White;
+            label18.Location = new Point(15, 15);
+            label18.Name = "label18";
+            label18.Size = new Size(78, 18);
+            label18.TabIndex = 0;
+            label18.Text = "Imports";
+            label18.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // panel6
+            // 
+            panel6.BackColor = Color.FromArgb(45, 62, 80);
+            panel6.BorderStyle = BorderStyle.FixedSingle;
+            panel6.Controls.Add(StockCountLabel);
+            panel6.Controls.Add(label6);
+            panel6.Font = new Font("Verdana", 12F, FontStyle.Bold);
+            panel6.Location = new Point(112, 221);
+            panel6.Name = "panel6";
+            panel6.Padding = new Padding(15);
+            panel6.Size = new Size(250, 90);
+            panel6.TabIndex = 10;
+            // 
+            // StockCountLabel
+            // 
+            StockCountLabel.Dock = DockStyle.Bottom;
+            StockCountLabel.Font = new Font("Verdana", 15.75F, FontStyle.Bold);
+            StockCountLabel.ForeColor = Color.FromArgb(27, 188, 155);
+            StockCountLabel.Location = new Point(15, 38);
+            StockCountLabel.Name = "StockCountLabel";
+            StockCountLabel.Size = new Size(218, 35);
+            StockCountLabel.TabIndex = 1;
+            StockCountLabel.Text = "100";
+            StockCountLabel.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Dock = DockStyle.Top;
+            label6.ForeColor = Color.White;
+            label6.Location = new Point(15, 15);
+            label6.Name = "label6";
+            label6.Size = new Size(56, 18);
+            label6.TabIndex = 0;
+            label6.Text = "Stock";
+            label6.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // panel5
+            // 
+            panel5.BackColor = Color.FromArgb(45, 62, 80);
+            panel5.BorderStyle = BorderStyle.FixedSingle;
+            panel5.Controls.Add(WarehousesCountLabel);
+            panel5.Controls.Add(label3);
+            panel5.Font = new Font("Verdana", 12F, FontStyle.Bold);
+            panel5.Location = new Point(112, 115);
+            panel5.Name = "panel5";
+            panel5.Padding = new Padding(15);
+            panel5.Size = new Size(250, 90);
+            panel5.TabIndex = 11;
+            // 
+            // WarehousesCountLabel
+            // 
+            WarehousesCountLabel.Dock = DockStyle.Bottom;
+            WarehousesCountLabel.Font = new Font("Verdana", 15.75F, FontStyle.Bold);
+            WarehousesCountLabel.ForeColor = Color.FromArgb(27, 188, 155);
+            WarehousesCountLabel.Location = new Point(15, 38);
+            WarehousesCountLabel.Name = "WarehousesCountLabel";
+            WarehousesCountLabel.Size = new Size(218, 35);
+            WarehousesCountLabel.TabIndex = 1;
+            WarehousesCountLabel.Text = "100";
+            WarehousesCountLabel.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Dock = DockStyle.Top;
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(15, 15);
+            label3.Name = "label3";
+            label3.Size = new Size(118, 18);
+            label3.TabIndex = 0;
+            label3.Text = "Warehouses";
+            label3.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Verdana", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(53, 344);
+            label1.Name = "label1";
+            label1.Size = new Size(132, 32);
+            label1.TabIndex = 17;
+            label1.Text = "Reports";
+            // 
+            // WarehouseReportButton
+            // 
+            WarehouseReportButton.BackColor = Color.FromArgb(45, 62, 80);
+            WarehouseReportButton.FlatAppearance.BorderColor = Color.FromArgb(22, 160, 134);
+            WarehouseReportButton.FlatAppearance.MouseDownBackColor = Color.FromArgb(22, 160, 134);
+            WarehouseReportButton.FlatAppearance.MouseOverBackColor = Color.FromArgb(22, 160, 134);
+            WarehouseReportButton.FlatStyle = FlatStyle.Flat;
+            WarehouseReportButton.Font = new Font("Verdana", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            WarehouseReportButton.Location = new Point(112, 415);
+            WarehouseReportButton.Name = "WarehouseReportButton";
+            WarehouseReportButton.Size = new Size(175, 177);
+            WarehouseReportButton.TabIndex = 0;
+            WarehouseReportButton.Text = "Warehouses";
+            WarehouseReportButton.UseVisualStyleBackColor = false;
+            WarehouseReportButton.Click += WarehouseReportButton_Click;
+            // 
+            // ProductReportButton
+            // 
+            ProductReportButton.BackColor = Color.FromArgb(45, 62, 80);
+            ProductReportButton.FlatAppearance.BorderColor = Color.FromArgb(22, 160, 134);
+            ProductReportButton.FlatAppearance.MouseDownBackColor = Color.FromArgb(22, 160, 134);
+            ProductReportButton.FlatAppearance.MouseOverBackColor = Color.FromArgb(22, 160, 134);
+            ProductReportButton.FlatStyle = FlatStyle.Flat;
+            ProductReportButton.Font = new Font("Verdana", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            ProductReportButton.Location = new Point(322, 415);
+            ProductReportButton.Name = "ProductReportButton";
+            ProductReportButton.Size = new Size(175, 177);
+            ProductReportButton.TabIndex = 1;
+            ProductReportButton.Text = "Products";
+            ProductReportButton.UseVisualStyleBackColor = false;
+            ProductReportButton.Click += ProductReportButton_Click;
+            // 
+            // TransactionsFormButton
+            // 
+            TransactionsFormButton.BackColor = Color.FromArgb(45, 62, 80);
+            TransactionsFormButton.FlatAppearance.BorderColor = Color.FromArgb(22, 160, 134);
+            TransactionsFormButton.FlatAppearance.MouseDownBackColor = Color.FromArgb(22, 160, 134);
+            TransactionsFormButton.FlatAppearance.MouseOverBackColor = Color.FromArgb(22, 160, 134);
+            TransactionsFormButton.FlatStyle = FlatStyle.Flat;
+            TransactionsFormButton.Font = new Font("Verdana", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            TransactionsFormButton.Location = new Point(532, 415);
+            TransactionsFormButton.Name = "TransactionsFormButton";
+            TransactionsFormButton.Size = new Size(175, 177);
+            TransactionsFormButton.TabIndex = 2;
+            TransactionsFormButton.Text = "Transactions\r\n";
+            TransactionsFormButton.UseVisualStyleBackColor = false;
+            TransactionsFormButton.Click += TransactionsFormButton_Click;
+            // 
+            // StockReportFormButton
+            // 
+            StockReportFormButton.BackColor = Color.FromArgb(45, 62, 80);
+            StockReportFormButton.FlatAppearance.BorderColor = Color.FromArgb(22, 160, 134);
+            StockReportFormButton.FlatAppearance.MouseDownBackColor = Color.FromArgb(22, 160, 134);
+            StockReportFormButton.FlatAppearance.MouseOverBackColor = Color.FromArgb(22, 160, 134);
+            StockReportFormButton.FlatStyle = FlatStyle.Flat;
+            StockReportFormButton.Font = new Font("Verdana", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            StockReportFormButton.Location = new Point(742, 415);
+            StockReportFormButton.Name = "StockReportFormButton";
+            StockReportFormButton.Size = new Size(175, 177);
+            StockReportFormButton.TabIndex = 3;
+            StockReportFormButton.Text = "Stock";
+            StockReportFormButton.UseVisualStyleBackColor = false;
+            StockReportFormButton.Click += StockReportFormButton_Click;
+            // 
+            // DashboardForm
+            // 
+            AutoScaleDimensions = new SizeF(96F, 96F);
+            AutoScaleMode = AutoScaleMode.Dpi;
+            AutoScroll = true;
+            BackColor = Color.FromArgb(52, 73, 94);
+            ClientSize = new Size(1020, 680);
+            Controls.Add(StockReportFormButton);
+            Controls.Add(TransactionsFormButton);
+            Controls.Add(ProductReportButton);
+            Controls.Add(WarehouseReportButton);
+            Controls.Add(label1);
+            Controls.Add(label19);
+            Controls.Add(panel8);
+            Controls.Add(panel11);
+            Controls.Add(panel7);
+            Controls.Add(panel12);
+            Controls.Add(panel6);
+            Controls.Add(panel5);
+            Font = new Font("Verdana", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            ForeColor = Color.White;
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "DashboardForm";
+            Padding = new Padding(50);
+            Text = "DashboardForm";
+            panel8.ResumeLayout(false);
+            panel8.PerformLayout();
+            panel11.ResumeLayout(false);
+            panel11.PerformLayout();
+            panel7.ResumeLayout(false);
+            panel7.PerformLayout();
+            panel12.ResumeLayout(false);
+            panel12.PerformLayout();
+            panel6.ResumeLayout(false);
+            panel6.PerformLayout();
+            panel5.ResumeLayout(false);
+            panel5.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label label19;
+        private Panel panel8;
+        private Label ClientsCountLabel;
+        private Label label10;
+        private Panel panel11;
+        private Label ExportsCountLabel;
+        private Label label16;
+        private Panel panel7;
+        private Label SuppliersCountLabel;
+        private Label label8;
+        private Panel panel12;
+        private Label ImportsCountLabel;
+        private Label label18;
+        private Panel panel6;
+        private Label StockCountLabel;
+        private Label label6;
+        private Panel panel5;
+        private Label WarehousesCountLabel;
+        private Label label3;
+        private Label label1;
+        private Button WarehouseReportButton;
+        private Button ProductReportButton;
+        private Button TransactionsFormButton;
+        private Button StockReportFormButton;
+    }
+}
---
+++ Forms/DashboardForm.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Warehouse_Management_App.Entities;
+
+namespace Warehouse_Management_App.Forms
+{
+    public partial class DashboardForm : Form
+    {
+        //
+        //  Fieds
+        //
+        AppDbContext m_appDbContext = new AppDbContext();
+
+
+        //
+        //  Constructors
+        //
+        public DashboardForm()
+        {
+            InitializeComponent();
+
+            // Get the overview info
+            WarehousesCountLabel.Text = (from W in m_appDbContext.Warehouses select W.ID).Count().ToString();
+            SuppliersCountLabel.Text = (from C in m_appDbContext.Contacts where C.Type == ContactType.Supplier select C.ID).Count().ToString();
+            ClientsCountLabel.Text = (from C in m_appDbContext.Contacts where C.Type == ContactType.Client select C.ID).Count().ToString();
+            StockCountLabel.Text = (from WP in m_appDbContext.WarehouseProducts select WP.ID).Count().ToString();
+            ImportsCountLabel.Text = (from I in m_appDbContext.Imports select I.ID).Count().ToString();
+            ExportsCountLabel.Text = (from E in m_appDbContext.Exports select E.ID).Count().ToString();
+        }
+
+
+        //
+        //  Event handlers
+        //
+        private void WarehouseReportButton_Click(object sender, EventArgs e)
+        {
+            var warehouseReportForm = new WarehousesReportForm();
+
+            warehouseReportForm.ShowDialog();
+        }
+
+        private void ProductReportButton_Click(object sender, EventArgs e)
+        {
+            var warehouseReportForm = new ProductsReportForm();
+
+            warehouseReportForm.ShowDialog();
+        }
+
+        private void TransactionsFormButton_Click(object sender, EventArgs e)
+        {
+            var warehouseReportForm = new TransactionsReportForm();
+
+            warehouseReportForm.ShowDialog();
+        }
+
+        private void StockReportFormButton_Click(object sender, EventArgs e)
+        {
+            var warehouseReportForm = new StockReportForm();
+
+            warehouseReportForm.ShowDialog();
+        }
+    }
+}
