bce1edd21f70d840a173a30a30121326ef7282d6
Omar Alaa <omar98alaa@gmail.com>
2025-06-05 02:20:49 +0300

    Product transactions form

---
+++ Forms/TransactionsReportForm.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace Warehouse_Management_App.Forms
+{
+    partial class TransactionsReportForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
+            WarehousesCheckedListBox = new CheckedListBox();
+            label1 = new Label();
+            label13 = new Label();
+            ReportDataGridView = new DataGridView();
+            ReportsComboBox = new ComboBox();
+            label7 = new Label();
+            ToDateTimePicker = new DateTimePicker();
+            FromDateTimePicker = new DateTimePicker();
+            label10 = new Label();
+            label9 = new Label();
+            ((System.ComponentModel.ISupportInitialize)ReportDataGridView).BeginInit();
+            SuspendLayout();
+            // 
+            // WarehousesCheckedListBox
+            // 
+            WarehousesCheckedListBox.BackColor = Color.FromArgb(45, 62, 80);
+            WarehousesCheckedListBox.BorderStyle = BorderStyle.FixedSingle;
+            WarehousesCheckedListBox.CheckOnClick = true;
+            WarehousesCheckedListBox.ForeColor = Color.White;
+            WarehousesCheckedListBox.FormattingEnabled = true;
+            WarehousesCheckedListBox.HorizontalScrollbar = true;
+            WarehousesCheckedListBox.Location = new Point(887, 92);
+            WarehousesCheckedListBox.Name = "WarehousesCheckedListBox";
+            WarehousesCheckedListBox.Size = new Size(328, 164);
+            WarehousesCheckedListBox.TabIndex = 0;
+            WarehousesCheckedListBox.ItemCheck += WarehousesCheckedListBox_ItemCheck;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Verdana", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(53, 50);
+            label1.Name = "label1";
+            label1.Size = new Size(461, 32);
+            label1.TabIndex = 111;
+            label1.Text = "Products Transactions Report";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Font = new Font("Verdana", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label13.Location = new Point(887, 73);
+            label13.Name = "label13";
+            label13.Size = new Size(146, 16);
+            label13.TabIndex = 109;
+            label13.Text = "Select Warehouses";
+            // 
+            // ReportDataGridView
+            // 
+            ReportDataGridView.AllowUserToAddRows = false;
+            ReportDataGridView.AllowUserToDeleteRows = false;
+            ReportDataGridView.AllowUserToResizeRows = false;
+            ReportDataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            ReportDataGridView.BackgroundColor = Color.FromArgb(45, 62, 80);
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = Color.FromArgb(45, 62, 80);
+            dataGridViewCellStyle1.Font = new Font("Verdana", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            dataGridViewCellStyle1.ForeColor = Color.White;
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            ReportDataGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            ReportDataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = SystemColors.Window;
+            dataGridViewCellStyle2.Font = new Font("Verdana", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dataGridViewCellStyle2.ForeColor = Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.False;
+            ReportDataGridView.DefaultCellStyle = dataGridViewCellStyle2;
+            ReportDataGridView.Location = new Point(97, 321);
+            ReportDataGridView.MultiSelect = false;
+            ReportDataGridView.Name = "ReportDataGridView";
+            ReportDataGridView.ReadOnly = true;
+            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle3.BackColor = SystemColors.Control;
+            dataGridViewCellStyle3.Font = new Font("Verdana", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dataGridViewCellStyle3.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = DataGridViewTriState.True;
+            ReportDataGridView.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle4.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle4.BackColor = Color.FromArgb(45, 62, 80);
+            dataGridViewCellStyle4.Font = new Font("Verdana", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dataGridViewCellStyle4.ForeColor = Color.White;
+            dataGridViewCellStyle4.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = Color.White;
+            ReportDataGridView.RowsDefaultCellStyle = dataGridViewCellStyle4;
+            ReportDataGridView.Size = new Size(1118, 346);
+            ReportDataGridView.TabIndex = 118;
+            // 
+            // ReportsComboBox
+            // 
+            ReportsComboBox.BackColor = Color.FromArgb(45, 62, 80);
+            ReportsComboBox.FlatStyle = FlatStyle.Flat;
+            ReportsComboBox.ForeColor = Color.White;
+            ReportsComboBox.FormattingEnabled = true;
+            ReportsComboBox.Location = new Point(97, 294);
+            ReportsComboBox.MaxDropDownItems = 5;
+            ReportsComboBox.Name = "ReportsComboBox";
+            ReportsComboBox.Size = new Size(322, 24);
+            ReportsComboBox.TabIndex = 116;
+            ReportsComboBox.SelectedIndexChanged += ReportsComboBox_SelectedIndexChanged;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Verdana", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.Location = new Point(97, 271);
+            label7.Name = "label7";
+            label7.Size = new Size(104, 16);
+            label7.TabIndex = 117;
+            label7.Text = "Select Report";
+            // 
+            // ToDateTimePicker
+            // 
+            ToDateTimePicker.Format = DateTimePickerFormat.Short;
+            ToDateTimePicker.Location = new Point(1054, 292);
+            ToDateTimePicker.Name = "ToDateTimePicker";
+            ToDateTimePicker.Size = new Size(161, 23);
+            ToDateTimePicker.TabIndex = 113;
+            ToDateTimePicker.ValueChanged += ToDateTimePicker_ValueChanged;
+            // 
+            // FromDateTimePicker
+            // 
+            FromDateTimePicker.Format = DateTimePickerFormat.Short;
+            FromDateTimePicker.Location = new Point(887, 292);
+            FromDateTimePicker.Name = "FromDateTimePicker";
+            FromDateTimePicker.Size = new Size(161, 23);
+            FromDateTimePicker.TabIndex = 112;
+            FromDateTimePicker.ValueChanged += FromDateTimePicker_ValueChanged;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Verdana", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label10.Location = new Point(1054, 273);
+            label10.Name = "label10";
+            label10.Size = new Size(24, 16);
+            label10.TabIndex = 115;
+            label10.Text = "To";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Verdana", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label9.Location = new Point(887, 273);
+            label9.Name = "label9";
+            label9.Size = new Size(45, 16);
+            label9.TabIndex = 114;
+            label9.Text = "From";
+            // 
+            // TransactionsReportForm
+            // 
+            AutoScaleDimensions = new SizeF(96F, 96F);
+            AutoScaleMode = AutoScaleMode.Dpi;
+            BackColor = Color.FromArgb(52, 73, 94);
+            ClientSize = new Size(1268, 720);
+            Controls.Add(ReportDataGridView);
+            Controls.Add(ReportsComboBox);
+            Controls.Add(label7);
+            Controls.Add(ToDateTimePicker);
+            Controls.Add(FromDateTimePicker);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(WarehousesCheckedListBox);
+            Controls.Add(label1);
+            Controls.Add(label13);
+            DoubleBuffered = true;
+            Font = new Font("Verdana", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            ForeColor = Color.White;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "TransactionsReportForm";
+            Padding = new Padding(50);
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "TransactionsReportForm";
+            ((System.ComponentModel.ISupportInitialize)ReportDataGridView).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private CheckedListBox WarehousesCheckedListBox;
+        private Label label1;
+        private Label label13;
+        private DataGridView ReportDataGridView;
+        private ComboBox ReportsComboBox;
+        private Label label7;
+        private DateTimePicker ToDateTimePicker;
+        private DateTimePicker FromDateTimePicker;
+        private Label label10;
+        private Label label9;
+    }
+}
---
+++ Forms/TransactionsReportForm.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Warehouse_Management_App.Entities;
+
+namespace Warehouse_Management_App.Forms
+{
+    public partial class TransactionsReportForm : Form
+    {
+        //
+        //  Fields
+        //
+        AppDbContext appDbContext = new AppDbContext();
+        List<Warehouse> warehouses;
+        DateTime fromDate;
+        DateTime toDate;
+
+
+        //
+        //  Constructors
+        //
+        public TransactionsReportForm()
+        {
+            InitializeComponent();
+
+            // Set to date to current date
+            FromDateTimePicker.MaxDate = DateTime.Now.Date;
+            ToDateTimePicker.MaxDate = DateTime.Now.Date;
+
+            // Set from date 30 days before
+            FromDateTimePicker.Value = DateTime.Now.AddDays(-30).Date;
+
+            // Add report types
+            ReportsComboBox.Items.AddRange(["Stock", "Imports", "Exports", "Transfers"]);
+
+            ReportsComboBox.SelectedIndex = 0;
+
+            // Load all warehouses
+            LoadWarehouses();
+        }
+
+
+        //
+        //  Methods
+        //
+        void LoadWarehouses()
+        {
+            warehouses = (from W in appDbContext.Warehouses orderby W.Name select W).ToList();
+
+            foreach (var W in warehouses)
+            {
+                WarehousesCheckedListBox.Items.Add(W.Name);
+            }
+        }
+
+        void UpdateStockDisplay()
+        {
+            // Get stock for all selected warehouses
+            var stock = (
+                from WP in appDbContext.WarehouseProducts
+                where WarehousesCheckedListBox.CheckedItems.Contains(WP.Warehouse.Name)
+                orderby WP.Warehouse.Name, WP.ProductionDate
+                select new
+                {
+                    Code = WP.ProductCode,
+                    Product_Name = WP.Product.Name,
+                    Warehouse_Name = WP.Warehouse.Name,
+                    Production_Date = WP.ProductionDate,
+                    Expiry_Date = WP.ProductionDate.AddDays(WP.Product.ExpiryPeriod).Date,
+                    Quantity = WP.Quantity
+                }
+                ).ToList();
+
+            ReportDataGridView.DataSource = stock;
+
+        }
+
+        void UpdateImportsDisplay()
+        {
+            // Get imports for all selected warehouses in the given period
+            var imports = (
+                from I in appDbContext.ImportedProducts
+                where WarehousesCheckedListBox.CheckedItems.Contains(I.Import.Warehouse.Name) &&
+                    fromDate.Date <= I.Import.IssueDate.Date &&
+                    I.Import.IssueDate.Date <= toDate.Date
+                orderby I.Import.IssueDate descending
+                select new
+                {
+                    Date = I.Import.IssueDate,
+                    Code = I.ProductCode,
+                    Product_Name = I.Product.Name,
+                    Warehouse_Name = I.Import.Warehouse.Name,
+                    Production_Date = I.ProductionDate,
+                    Expiry_Date = I.ProductionDate.AddDays(I.Product.ExpiryPeriod).Date,
+                    Quantity = I.Quantity,
+                    Supplier = I.Import.Contact.Email,
+                }
+                ).ToList();
+
+            ReportDataGridView.DataSource = imports;
+        }
+
+        void UpdateExportsDisplay()
+        {
+            // Get exports for all selected warehouses in the given period
+            var exports = (
+                from I in appDbContext.ExportedProducts
+                where WarehousesCheckedListBox.CheckedItems.Contains(I.Export.Warehouse.Name) &&
+                    fromDate.Date <= I.Export.IssueDate.Date &&
+                    I.Export.IssueDate.Date <= toDate.Date
+                orderby I.Export.IssueDate descending
+                select new
+                {
+                    Date = I.Export.IssueDate,
+                    Code = I.ProductCode,
+                    Product_Name = I.Product.Name,
+                    Warehouse_Name = I.Export.Warehouse.Name,
+                    Quantity = I.Quantity,
+                    Client = I.Export.Contact.Email,
+                }
+                ).ToList();
+
+            ReportDataGridView.DataSource = exports;
+        }
+
+        void UpdateTransfersDisplay()
+        {
+            // Get transfered for all selected warehouses in the given period
+            var transfers = (
+                from I in appDbContext.TransferedProducts
+                where (WarehousesCheckedListBox.CheckedItems.Contains(I.Transfer.SourceWarehouse.Name) ||
+                    WarehousesCheckedListBox.CheckedItems.Contains(I.Transfer.DestinationWarehouse.Name)) &&
+                    fromDate.Date <= I.Transfer.IssueDate.Date &&
+                    I.Transfer.IssueDate.Date <= toDate.Date
+                orderby I.Transfer.IssueDate descending
+                select new
+                {
+                    Date = I.Transfer.IssueDate,
+                    Code = I.ProductCode,
+                    Product_Name = I.Product.Name,
+                    Source_Warehouse_Name = I.Transfer.SourceWarehouse.Name,
+                    Destination_Warehouse_Name = I.Transfer.DestinationWarehouse.Name,
+                    Production_Date = I.ProductionDate,
+                    Expiry_Date = I.ProductionDate.AddDays(I.Product.ExpiryPeriod).Date,
+                    Quantity = I.Quantity,
+                    Supplier = I.Transfer.Contact.Email,
+                }
+                ).ToList();
+
+            ReportDataGridView.DataSource = transfers;
+        }
+
+        void RefreshReportDisplay()
+        {
+            var idx = ReportsComboBox.SelectedIndex;
+            ReportsComboBox.SelectedIndex = -1;
+            ReportsComboBox.SelectedIndex = idx;
+        }
+
+        //
+        //  Event handlers
+        //
+        private void FromDateTimePicker_ValueChanged(object sender, EventArgs e)
+        {
+            // Update the period lower limit
+            fromDate = FromDateTimePicker.Value.Date;
+            ToDateTimePicker.MinDate = fromDate;
+
+            RefreshReportDisplay();
+        }
+
+        private void ToDateTimePicker_ValueChanged(object sender, EventArgs e)
+        {
+            // Update the period upper limit
+            toDate = ToDateTimePicker.Value.Date;
+            FromDateTimePicker.MaxDate = toDate;
+
+            RefreshReportDisplay();
+        }
+
+        private void WarehousesCheckedListBox_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            // Update displayed data to include/exclude a warehouse
+            this.BeginInvoke(() =>
+            {
+                RefreshReportDisplay();
+            });
+
+        }
+
+        private void ReportsComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Ensure date pick is enabled
+            FromDateTimePicker.Enabled = true;
+            ToDateTimePicker.Enabled = true;
+
+            // Display the selected report type
+            switch (ReportsComboBox.SelectedIndex)
+            {
+                case 0:
+                    // Disable date pick for stock display only
+                    FromDateTimePicker.Enabled = false;
+                    ToDateTimePicker.Enabled = false;
+
+                    UpdateStockDisplay();
+                    break;
+
+                case 1:
+                    UpdateImportsDisplay();
+                    break;
+
+                case 2:
+                    UpdateExportsDisplay();
+                    break;
+
+                case 3:
+                    UpdateTransfersDisplay();
+                    break;
+            }
+        }
+    }
+}
